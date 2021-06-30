a0dc77808de1136d0440f3c0472f2a422062f4b4
Uguen Bernard <bernard.uguen@univ-rennes1.fr>
2021-06-30 17:12:28 +0200

    extract tag tid information

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pandas as pd
 url = 'https://raw.githubusercontent.com/buguen/tid/master/tag_model.csv'
-df = pd.read_csv(url)
+df = pd.read_csv(url,index_col='num')
+tag = df.loc['0x1170']
 
